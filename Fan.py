1e129e59114ae3df4f1622187e21d22f76a599b7
SimoneCarena <simonecarena2000@gmail.com>
2024-04-05 18:34:07 +0200

    Created Fan class

---
+++ Fan.py
@@ -0,0 +1,35 @@
+import numpy as np
+
+class Fan:
+    '''
+    Class describing the fans generating the wind. The parameters
+    are the position of the fan p0=(x0,y0), the orientation of the fan
+    u0=(ux,uy), the spread of the fan theta and the initial wind speed
+    '''
+    def __init__(self,x0,y0,ux,uy,theta,v0):
+        self.p0 = np.array([x0,y0])
+        self.theta = theta
+        self.u0 = np.array([ux,uy])
+        self.v0 = v0
+
+    def generate_wind(self,x,y):
+        '''
+        Method used to generate the wind at a certain location (x,y).
+        The function returns the wind speed at the specified coordinates
+        and the scale used in the quiver plot
+        '''
+        d = (x-self.p0[0])**2+(y-self.p0[1])**2
+        u = np.array([(x-self.p0[0]),(y-self.p0[1])])
+        u = u/np.linalg.norm(u)
+        # Scale the speed with ditance
+        distance_scale = 1+d
+        # Check if the point is inside the cone
+        p = np.sqrt(d)*self.u0+np.array([self.p0[0],self.p0[1]])
+        # The angle is computed using traingles properties
+        alpha = 2*np.arcsin(np.sqrt((x-p[0])**2+(y-p[1])**2)/(2*np.sqrt(d)))
+        if alpha < self.theta:
+            speed = self.v0/distance_scale*np.copy(self.u0)
+        else:
+            speed = np.array([0,0])
+        return speed, distance_scale
+        
--- wind.py
+++ wind.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import json
+from Fan import Fan
 
-max_scale = 10
+max_scale=10
 initial_headwidth = 3
 initial_headlenght = 4
 theta = np.deg2rad(30)
@@ -9,26 +11,21 @@ theta = np.deg2rad(30)
 plt.xlim([0, 4])
 plt.ylim([0, 4])
 
-x0 = 4
-y0 = 2
-direction = np.array([-1,0])
+x0 = 2
+y0 = 4
+direction = np.array([np.sqrt(2)/2,-np.sqrt(2)/2])
 
 plt.plot(x0,y0,"ko",markersize=8)
 plt.quiver(x0,y0,direction[0],direction[1],color='k',scale=max_scale)
 
+fan = Fan(x0,y0,direction[0],direction[1],theta,5)
+
 for x in np.linspace(0,4,20):
-    for y in np.linspace(0,4,30):
-        d = (x-x0)**2+(y-y0)**2
-        u = np.array([(x-x0),(y-y0)])
-        u = u/np.linalg.norm(u)
-        # Scale the speed with ditance
-        distance_scale = 1+d
-        # Check if the point is inside the cone
-        p0 = np.sqrt(d)*direction+np.array([x0,y0])
-        alpha = 2*np.arcsin(np.sqrt((x-p0[0])**2+(y-p0[1])**2)/(2*np.sqrt(d)))
-        if alpha > theta:
+    for y in np.linspace(0,4,20):
+        speed, scale = fan.generate_wind(x,y)
+        if speed[0]==0 and speed[1]==0:
             continue
-        scale = max_scale*distance_scale #This scale is to draw the lines only
-        plt.quiver(x,y,u[0],u[1],scale=scale,width=0.0035,color='r')
+        speed = speed/np.linalg.norm(speed)
+        plt.quiver(x,y,speed[0],speed[1],width=0.0035,color='r',scale=max_scale*scale)
 
 plt.show()
