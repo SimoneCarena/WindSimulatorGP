0f8b4845ca5863e927c09ace2cfcb61f383e69d1
SimoneCarena <simonecarena2000@gmail.com>
2024-07-18 16:47:07 +0200

    The mpc seems to be working, NOW WITH CONSTRAINTS

--- modules/MPC.py
+++ modules/MPC.py
@@ -1,19 +1,18 @@
 import numpy as np
 import casadi as ca
 
-class MPCController:
-    def __init__(self, quadrotor, prediction_horizon, control_horizon, dt, Q, R):
-        self.quadrotor = quadrotor
-        self.prediction_horizon = prediction_horizon
+class MPC:
+    def __init__(self, dynamics, control_horizon, dt, Q, R):
+        self.dynamics = dynamics
         self.control_horizon = control_horizon
         self.dt = dt
 
         # Define optimization variables
         self.u = ca.SX.sym('u', 4, control_horizon)
-        self.p = ca.SX.sym('p',10+4) # Initial State + Reference
+        self.p = ca.SX.sym('p',10+6) # Initial State + Reference
 
         self.nx = 10 # State Dimension
-        self.ny = 4 # Tracking Dimension
+        self.ny = 6 # Tracking Dimension
         self.nu = 4 # Control Dimension
 
         # Define the cost function to focus on the first 5 state elements
@@ -23,25 +22,35 @@ class MPCController:
 
     def define_optimization_problem(self):
         cost = 0
-        constraints = []
+        # Equality Constratints
+        g = []
+        # Inequality Constraints on the optimization variable
+        lower_bound = []
+        upper_bound = []
 
         # Initialize the state trajectory
         x_t = self.p[:10]
         ref = self.p[10:]
         for t in range(self.control_horizon):
             # Compute cost
-            cost += (ref-x_t[6:]).T@self.Q@(ref-x_t[6:]) + self.u[:,t].T@self.R@self.u[:,t]
+            cost += (ref-x_t[:self.ny]).T@self.Q@(ref-x_t[:self.ny]) + self.u[:,t].T@self.R@self.u[:,t]
 
             # Compute next state using RK4 integration
-            x_next = self.rk4_integration(x_t, self.u[:, t], self.dt)
+            x_next = self.__step(x_t, self.u[:, t], self.dt)
             x_t = x_next
 
-        # Formulate the NLP
-        nlp = {'x': ca.reshape(self.u, -1, 1), 'f': cost, 'g': ca.vertcat(*constraints), 'p': self.p}
-        opts = {'ipopt.print_level':0, 'print_time':0}
-        self.solver = ca.nlpsol('solver', 'ipopt', nlp, opts)
+            lower_bound += [-np.pi/6,-np.pi/6,-np.pi/6,5]
+            upper_bound += [np.pi/6,np.pi/6,np.pi/6,15]
 
-    def rk4_integration(self, x, u, dt):
+        self.lower_bound = ca.vertcat(lower_bound)
+        self.upper_bound = ca.vertcat(upper_bound)
+
+        # Formulate the NLP
+        nlp = {'x': ca.reshape(self.u, -1, 1), 'f': cost, 'g': ca.vertcat(*g), 'p': self.p}
+        opts = {'ipopt.print_level':0, 'print_time':0}
+        self.solver = ca.nlpsol('solver', 'ipopt', nlp)
+
+    def __step(self, x, u, dt):
         """
         Perform a single RK4 integration step.
         
@@ -50,16 +59,23 @@ class MPCController:
         :param dt: Time step
         :return: State after one RK4 step
         """
-        k1 = self.quadrotor.ideal_dynamics(x, u)
-        k2 = self.quadrotor.ideal_dynamics(x + dt/2 * k1, u)
-        k3 = self.quadrotor.ideal_dynamics(x + dt/2 * k2, u)
-        k4 = self.quadrotor.ideal_dynamics(x + dt * k3, u)
+        k1 = self.dynamics(x, u)
+        k2 = self.dynamics(x + dt/2 * k1, u)
+        k3 = self.dynamics(x + dt/2 * k2, u)
+        k4 = self.dynamics(x + dt * k3, u)
         x_next = x + dt/6 * (k1 + 2*k2 + 2*k3 + k4)
         return x_next
 
-    def compute_control_input(self, x0, ref):
+    def __call__(self, x, ref):
         # Solve the optimization problem
-        initial_guess = np.zeros(4 * self.control_horizon)
-        sol = self.solver(x0=initial_guess, p=ca.vertcat(x0,ref), lbg=0, ubg=0)
-        u_opt = sol['x'].reshape((4, self.control_horizon))
+        initial_guess = np.zeros(self.nu * self.control_horizon)
+        sol = self.solver(
+            x0=initial_guess, 
+            p=ca.vertcat(x,ref), 
+            lbx=self.lower_bound, 
+            ubx=self.upper_bound,
+            lbg = 0,
+            ubg = 0
+        )
+        u_opt = sol['x'].reshape((self.nu, self.control_horizon))
         return u_opt[:, 0]
--- test.py
+++ test.py
@@ -1,24 +1,150 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 from modules.Quadrotor import Quadrotor
-from modules.MPC import MPCController
+from modules.MPC import MPC
 
 # Instantiate the Quadrotor and MPCController
 dt = 0.1
 x0 = np.zeros(10)
 quadrotor = Quadrotor(dt, x0)
-mpc_controller = MPCController(quadrotor, prediction_horizon=10, control_horizon=5, dt=dt, Q=np.eye(4), R=np.eye(4))
+mpc = MPC(
+    quadrotor.ideal_dynamics,
+    control_horizon=5, 
+    dt=dt, 
+    Q=100*np.diag([5,5,5,1,1,1]), 
+    R=np.eye(4)
+)
 
 # Example usage in a simulation loop
-time_steps = 100
-control_inputs = []
-states = [x0]
+time_steps = 200
+states = []
+T = []
+u_phi = []
+u_theta = []
+u_psi = []
+u_a = []
 
 for t in range(time_steps):
-    x_current = states[-1]
-    u = mpc_controller.compute_control_input(x_current,np.array([0,10,0,11]))
-    print(u)
-    control_inputs.append(u)
+    x_current = quadrotor.get_state()
+    states.append(x_current)
+    u = mpc(x_current,np.array([2,2,2,0,0,0]))
+    u_phi.append(np.array(u[0]).item())
+    u_theta.append(np.array(u[1]).item())
+    u_psi.append(np.array(u[2]).item())
+    u_a.append(np.array(u[3]).item())
     quadrotor.step(u, np.zeros(3))  # Assuming no wind disturbance for ideal dynamics
-    states.append(quadrotor.get_state())
+    T.append(t*dt)
 
-control_inputs, states
+xs = []
+ys = []
+zs = []
+vxs = []
+vys = []
+vzs = []
+for state in states:
+    xs.append(np.array(state[0]).item())
+    ys.append(np.array(state[1]).item())
+    zs.append(np.array(state[2]).item())
+    vxs.append(np.array(state[3]).item())
+    vys.append(np.array(state[4]).item())
+    vzs.append(np.array(state[5]).item())
+
+xs = np.array(xs)
+ys = np.array(ys)
+zs = np.array(zs)
+
+max_z = np.max(zs)
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+fig.set_size_inches(16,9)
+fig.tight_layout(pad=5)
+fig.suptitle('System Trajectory')
+ax.plot(xs,ys,zs,'-',color='c',label='System Trajectory')
+ax.plot(xs[0],ys[0],zs[0],'bo',label='Starting Position')
+ax.plot(xs[-1],ys[-1],zs[-1],'ro',label='End Position')
+ax.set_xlabel(r'$x$ $[m]$')
+ax.set_ylabel(r'$y$ $[m]$')
+ax.set_zlabel(r'$z$ $[m]$')
+ax.legend()
+plt.show()
+
+fig, ax = plt.subplots(1,2)
+fig.set_size_inches(16,9)
+fig.tight_layout(pad=5)
+ax[0].plot(T,xs,label='System Position x')
+ax[0].plot([T[0],T[-1]],[2,2],label='Reference Position')
+ax[0].legend()
+ax[1].plot(T,vxs,label='System Velocity x')
+ax[1].plot([T[0],T[-1]],[0,0],label='Reference Position')
+ax[1].legend()
+ax[0].set_xlabel(r'$t$ $[s]$')
+ax[0].set_ylabel(r'$x$ $[m]$')
+ax[1].set_xlabel(r'$t$ $[s]$')
+ax[1].set_ylabel(r'$v_x$ $[m/s]$')
+ax[0].set_xlim([0.0,time_steps*dt])
+ax[1].set_xlim([0.0,time_steps*dt])
+plt.show()
+
+fig, ax = plt.subplots(1,2)
+fig.set_size_inches(16,9)
+fig.tight_layout(pad=5)
+ax[0].plot(T,ys,label='System Position y')
+ax[0].plot([T[0],T[-1]],[2,2],label='Reference Position')
+ax[0].legend()
+ax[1].plot(T,vys,label='System Velocity y')
+ax[1].plot([T[0],T[-1]],[0,0],label='Reference Position')
+ax[1].legend()
+ax[0].set_xlabel(r'$t$ $[s]$')
+ax[0].set_ylabel(r'$y$ $[m]$')
+ax[1].set_xlabel(r'$t$ $[s]$')
+ax[1].set_ylabel(r'$v_y$ $[m/s]$')
+ax[0].set_xlim([0.0,time_steps*dt])
+ax[1].set_xlim([0.0,time_steps*dt])
+plt.show()
+
+fig, ax = plt.subplots(1,2)
+fig.set_size_inches(16,9)
+fig.tight_layout(pad=5)
+ax[0].plot(T,xs,label='System Position z')
+ax[0].plot([T[0],T[-1]],[2,2],label='Reference Position')
+ax[0].legend()
+ax[1].plot(T,vxs,label='System Velocity z')
+ax[1].plot([T[0],T[-1]],[0,0],label='Reference Position')
+ax[1].legend()
+ax[0].set_xlabel(r'$t$ $[s]$')
+ax[0].set_ylabel(r'$z$ $[m]$')
+ax[1].set_xlabel(r'$t$ $[s]$')
+ax[1].set_ylabel(r'$v_z$ $[m/s]$')
+ax[0].set_xlim([0.0,time_steps*dt])
+ax[1].set_xlim([0.0,time_steps*dt])
+plt.show()
+
+fig, ax = plt.subplots()
+fig.set_size_inches(16,9)
+fig.tight_layout(pad=5)
+fig.suptitle('Attitude Command')
+ax.plot(T,u_phi,label=r'$\phi^{c}$')
+ax.plot(T,u_theta,label=r'$\theta^{c}$')
+ax.plot(T,u_psi,label=r'$\psi^{c}$')
+ax.plot([T[0],T[-1]],[-np.pi/6,-np.pi/6],'k--',label='Bounds')
+ax.plot([T[0],T[-1]],[np.pi/6,np.pi/6],'k--')
+ax.set_xlabel(r'$t$ $[s]$')
+ax.set_ylabel(r'angle $[rad]$')
+ax.set_xlim([0.0,time_steps*dt])
+ax.legend()
+plt.show()
+
+fig, ax = plt.subplots()
+fig.set_size_inches(16,9)
+fig.tight_layout(pad=5)
+fig.suptitle('Thrust Command')
+ax.plot(T,u_a,label=r'$a^{c}$')
+ax.plot([T[0],T[-1]],[5,5],'k--',label='Bounds')
+ax.plot([T[0],T[-1]],[15,15],'k--')
+ax.set_xlabel(r'$t$ $[s]$')
+ax.set_ylabel(r'thrust $[m/s^2]$')
+ax.set_xlim([0.0,time_steps*dt])
+ax.legend()
+plt.show()
